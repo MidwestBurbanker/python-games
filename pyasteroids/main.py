d79eb03fea536ea8fb0360d86e960a76bc5fbc4d
samuelutley <samuelutley1@gmail.com>
2026-09-18 23:27:42 -0700

    Updated 'main' to include object groups. Put players into group containers to simplify code calling.

--- pyasteroids/main.py
+++ pyasteroids/main.py
@@ -7,6 +7,9 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280")
@@ -17,16 +20,22 @@ def main():
 
     while True:
         log_state()
-        dt = clock.tick(60) / 1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    return
+                return
+
+        updatable.update(dt)
 
         screen.fill("black")
-        playership.draw(screen)
-        playership.update(dt)
+
+        for space_objects in drawable:
+            space_objects.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 
